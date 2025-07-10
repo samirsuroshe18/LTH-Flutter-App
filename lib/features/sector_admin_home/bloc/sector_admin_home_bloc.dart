4c214c002f361c42a2bef92d0633a7fad238993d
Maitrey Khedekar <maitreykhedekar21@gmail.com>
2025-07-10 16:58:49 +0530

    Added create_worker and workers_list screens in sector_admin

--- lib/features/sector_admin_home/bloc/sector_admin_home_bloc.dart
+++ lib/features/sector_admin_home/bloc/sector_admin_home_bloc.dart
@@ -5,7 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:complaint_portal/features/auth/models/user_model.dart';
-
+import '../models/technician_model.dart';
 import '../../../utils/api_error.dart';
 import '../repository/sector_admin_home_repository.dart';
 
@@ -37,6 +37,36 @@ class SectorAdminHomeBloc extends Bloc<SectorAdminHomeEvent, SectorAdminHomeStat
         }
       }
     });
-
+    on<CreateWorkerEvent>((event, emit) async {
+      emit(CreateWorkerLoading());
+      try {
+        final technician = await _sectorAdminHomeRepository.createTechnician(
+          userName: event.userName,
+          email: event.email,
+          phoneNo: event.phoneNo,
+          technicianType: event.technicianType,
+        );
+        emit(CreateWorkerSuccess(technician: technician));
+      } catch (e) {
+        if (e is ApiError) {
+          emit(CreateWorkerFailure(message: e.message.toString()));
+        } else {
+          emit(CreateWorkerFailure(message: e.toString()));
+        }
+      }
+    });
+    on<GetWorkersListEvent>((event, emit) async {
+      emit(GetWorkersListLoading());
+      try {
+        final workers = await _sectorAdminHomeRepository.getWorkersList();
+        emit(GetWorkersListSuccess(workers: workers));
+      } catch (e) {
+        if (e is ApiError) {
+          emit(GetWorkersListFailure(message: e.message.toString()));
+        } else {
+          emit(GetWorkersListFailure(message: e.toString()));
+        }
+      }
+    });
   }
 }
